10653bc3c3c5eb8fa818fd730624bf6faa5d0f7c
karlaGM <elizabe130215@gmail.com>
2026-06-17 11:47:32 -0600

    Se agrega funcionalidad menu,modificacion_dom_Creaccion de paginas

---
+++ main.js
@@ -0,0 +1,44 @@
+ /* CREACIÓN DEL HEADER (Tu código) */
+const header = document.createElement('header');
+header.className = 'sticky-top shadow-sm';
+
+header.innerHTML = `
+   <nav class="navbar navbar-expand-lg navbar-white bg-white border-bottom py-3 shadow-sm">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav gap-3">
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold text-dark" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-primary fw-semibold" href="about.html">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-secondary" href="proyectos.html">Proyectos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+`;
+
+
+document.body.prepend(header);
+
+const footer = document.createElement('footer');
+footer.className = 'bg-white border-top py-4 mt-5 text-center';
+footer.innerHTML = `
+        <div class="container text-muted">
+            <p class="mb-1 fw-medium" style="font-size: 0.9rem;">&copy; 2026 - Karla Elizabeth Gonzalez Mondragon</p>
+            <p class="small mb-0 text-secondary">Desarrolladora Full Stack | Java & JavaScript</p>
+        </div>
+    </footer>`;
+
+document.body.appendChild(footer);
+
+
+ 
